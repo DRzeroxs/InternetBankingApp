0e39dd5000135ca555d0e96c22fc80310a85a7c4
DRzeroxs <mgrullonreinoso@gmail.com>
2024-03-08 23:27:57 -0400

    Agregar El string conexion

---
+++ InternetBankingApp.Core.Domain/Common/AuditableBaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniRedSocial.Core.Domain.Common
+{
+    public class AuditableBaseEntity
+    {
+        [Key]
+        public virtual int Id { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? LastModifiedby { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
