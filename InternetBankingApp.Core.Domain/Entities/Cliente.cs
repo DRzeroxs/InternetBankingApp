77484caf26a59fd47517c5999db3764e3d7f2d7a
DRzeroxs <mgrullonreinoso@gmail.com>
2024-03-09 15:58:53 -0400

    Creacion de Entities Borrador#1

---
+++ InternetBankingApp.Core.Domain/Entities/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using InternetBankingApp.Core.Domain.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InternetBankingApp.Core.Domain.Entities
+{
+    public class Cliente : AuditableBaseEntity
+    {
+        public string UserID { get; set; }
+
+        //conductores
+        [InverseProperty("Cliente")]
+        public ICollection<Transaccion> Transacciones { get; set; }
+
+        [InverseProperty("Cliente")]
+        public ICollection<TarjetaDeCredito> TarjetasDeCredito { get; set; }
+
+        [InverseProperty("Cliente")]
+        public ICollection<Prestamo> Prestamo { get; set; }
+
+        [InverseProperty("Cliente")]
+        public ICollection<CuentaDeAhorro> CuentasDeAhorro { get; set; }
+
+        [InverseProperty("Cliente")]
+        public ICollection<Beneficiario> Beneficiarios { get; set; }
+    }
+}
