04790c999fb24878dd699caf48daf6351d520465
DRzeroxs <mgrullonreinoso@gmail.com>
2024-03-12 16:18:02 -0400

    Creacion de Migraciones y Repositorios

---
+++ InternetBankingApp.Core.Application/Helpers/IdentifierGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBankingApp.Core.Application.Helpers
+{
+    public static class IdentifierGenerator
+    {
+        private static Random random = new Random();
+        
+        public static int GenerateCode(HashSet<int> generatedCodes)
+        {
+            while (true)
+            {
+                int code = random.Next(100000000, 1000000000);
+
+                if (!generatedCodes.Contains(code))
+                {
+                    return code;
+                }
+            }
+        }
+    }
+}
---
+++ InternetBankingApp.Infrastructure.Persistence/Repositories/TarjetaDeCreditoRepository.cs
@@ -0,0 +1,21 @@
+﻿using InternetBankingApp.Core.Application.Interfaces.IRepository;
+using InternetBankingApp.Core.Domain.Entities;
+using InternetBankingApp.Infrastructure.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBankingApp.Infrastructure.Persistence.Repositories
+{
+    public class TarjetaDeCreditoRepository : GenericRepository<TarjetaDeCredito>, ITarjetaDeCreditoRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public TarjetaDeCreditoRepository (ApplicationContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
