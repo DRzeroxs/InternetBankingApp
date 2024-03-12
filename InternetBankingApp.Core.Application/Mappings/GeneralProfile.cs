4af5d6a5941e9cc00cab9306ac71c7805393bd09
DRzeroxs <mgrullonreinoso@gmail.com>
2024-03-12 00:55:26 -0400

    Mapper de Beneficiario

--- InternetBankingApp.Core.Application/Mappings/GeneralProfile.cs
+++ InternetBankingApp.Core.Application/Mappings/GeneralProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using InternetBankingApp.Core.Application.Dtos.Account;
+using InternetBankingApp.Core.Application.ViewModels.Beneficiario;
 using InternetBankingApp.Core.Application.ViewModels.User;
+using InternetBankingApp.Core.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +13,31 @@ namespace InternetBankingApp.Core.Application.Mappings
 {
     public class GeneralProfile : Profile
     {
-
-        #region Beneficiario
-            
-        #endregion
-
         public GeneralProfile() 
         {
+
+
+            #region Beneficiario
+
+            CreateMap<Beneficiario, SaveBeneficiarioViewModel>()
+                .ReverseMap()
+                .ForMember(x => x.CreatedDate, opt => opt.Ignore())
+                .ForMember(x => x.LastModifiedDate, opt => opt.Ignore())
+                .ForMember(x => x.CreatedBy, opt => opt.Ignore())
+                .ForMember(x => x.LastModifiedby, opt => opt.Ignore())
+                .ForMember(x => x.Cliente, opt => opt.Ignore())
+                .ForMember(x => x.CuentaDeAhorro, opt => opt.Ignore());
+
+            CreateMap<Beneficiario, BeneficiarioViewModel>()
+                .ReverseMap()
+                .ForMember(x => x.CreatedDate, opt => opt.Ignore())
+                .ForMember(x => x.LastModifiedDate, opt => opt.Ignore())
+                .ForMember(x => x.CreatedBy, opt => opt.Ignore())
+                .ForMember(x => x.LastModifiedby, opt => opt.Ignore())
+                .ForMember(x => x.Cliente, opt => opt.Ignore())
+                .ForMember(x => x.CuentaDeAhorro, opt => opt.Ignore());
+            #endregion
+
             #region "User"
             CreateMap<AuthenticationRequest, LoginViewModel>()
                 .ForMember(x => x.HasError, opt => opt.Ignore())
