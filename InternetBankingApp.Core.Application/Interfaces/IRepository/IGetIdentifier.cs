df6a786ce9ec10cd9a68b0d37300f337f982b56d
DRzeroxs <mgrullonreinoso@gmail.com>
2024-03-16 05:42:08 -0400

    Cambios menores

---
+++ InternetBankingApp.Core.Application/Interfaces/IRepository/IGetIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using InternetBankingApp.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBankingApp.Core.Application.Interfaces.IRepository
+{
+    public interface IGetIdentifier<Entity> where Entity : class
+    {
+        Task<Entity> GetByIdentifierAsync(int identifier);
+        Task<List<int>> GetAllIdentifiersAsync();
+    }
+}
---
+++ InternetBankingApp.Core.Application/Interfaces/IServices/IGetIdentifierService.cs
@@ -0,0 +1,15 @@
+﻿using InternetBankingApp.Core.Application.ViewModels.CuentaDeAhorro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBankingApp.Core.Application.Interfaces.IServices
+{
+    public interface IGetIdentifierService<ViewModel> where ViewModel : class
+    {
+        Task<ViewModel> GetByIdentifier(int identifier);
+        Task<List<int>> GetAllIdentifiers();
+    }
+}
