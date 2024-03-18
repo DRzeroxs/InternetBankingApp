9cf5072f563129739a515e0e7a8e4cf7d2f24523
DRzeroxs <mgrullonreinoso@gmail.com>
2024-03-18 13:50:17 -0400

    Creacion de Enum para tipo de pago

---
+++ InternetBankingApp.Core.Application/Enums/ETipoPago.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBankingApp.Core.Application.Enums
+{
+    public enum ETipoPago
+    {
+        None = 0,
+        PagoExpreo = 1,
+        PagoTarjeta = 2,
+        PagoPretamo = 3,
+        PagoBeneficiario =4,
+    }
+}
